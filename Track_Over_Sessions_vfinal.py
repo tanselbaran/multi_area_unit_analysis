9d95c29ff04b59d7184d0fd147c661de15c7bec8
tanselbaran <yasart@ethz.ch>
2021-07-07 11:40:51 +0200

    Changed directory for input files

--- Track_Over_Sessions_vfinal.py
+++ Track_Over_Sessions_vfinal.py
@@ -20,9 +20,8 @@ import scipy.io
 from scipy import signal
 
 folder = os.getcwd()
-os.chdir('/home/baran/Desktop/github/unit_tracking/') 
-import Track_Functions   as TF
-os.chdir('/home/baran/Desktop/Chris_data/mouseLL2/sorted_spike_data') 
+from Track_Functions import Track_Functions   as TF
+os.chdir('/home/baran/Desktop/mouseLL2_spike_sorting_results/_sorted_spike_data') 
 # %% Extract Spikes from Structured Data file
 
 files = os.listdir()
@@ -41,11 +40,11 @@ Print_S_Opt = 0    # Choose: 0 - No print
 n_pca    = 3     # Number of Principal components if Suboption = 0
 perc_pca = 0.9  # Percentage of variance in pca  if Subotpion = 1
 
-# Extract n = all channels with highest amplitude from the averages. 
+# Extract n = all channels with highest amplitude from the averages.
 n_chan            = len(Waveforms[0][0][0]) # Number of Channels
 Waveforms_HA_c    = TF.extract_Allwav(Waveforms) # Concatenated Waves
 Waveforms_Allsamp = TF.conc_Allsamp(Waveforms_HA_c) # All Spike-waves
-    
+
 # Create PC-Subspace and project clusters onto it.
 
 pca = PCA(n_components=perc_pca)
@@ -54,20 +53,20 @@ scaler = MaxAbsScaler()
 with parallel_backend('threading', n_jobs=6):
     Waveforms_Allsamp_standardized = scaler.fit_transform(Waveforms_Allsamp)
     Proj = pca.fit(Waveforms_Allsamp_standardized)
-    
 
-Proj = [] 
+
+Proj = []
 for i in range(len(Waveforms_HA_c)):
     with parallel_backend('threading', n_jobs=6):
         Waveforms_HA_c_standardized = scaler.fit_transform(Waveforms_HA_c[i])
         Proj.append(np.transpose(pca.transform(Waveforms_HA_c_standardized)))
-     
+
 # Plot Clusters if we have 2- or 3-D space
 #if Suboption == 0:
 #    ax = TF.ClustPlot(n_pca, Proj)
-    
+
 # %% Data from PCA and PC-Subspace
-        
+
 Variance_Ratio = pca.explained_variance_ratio_
 S_Values       = pca.singular_values_
 Tot_Var        = np.sum(Variance_Ratio)
@@ -76,31 +75,30 @@ Tot_Var        = np.sum(Variance_Ratio)
 
 if Print_S_Opt == 0:   # No Plot
     print('No spike waveforms plotted.')
-    
+
 elif Print_S_Opt == 1: # Plot a set of single spikes in different figures
     TF.PlotSingleWaveforms(Waveforms, av_Waveforms, Time, cl = 0)
-    
-elif Print_S_Opt == 2 or Print_S_Opt == 3: # Plot concatenated waveforms  
+
+elif Print_S_Opt == 2 or Print_S_Opt == 3: # Plot concatenated waveforms
     Time = np.linspace(0,2*n_chan,Waveforms_HA_c[0].shape[1])
     Waveforms_HA_c_av = TF.extract_HAwav_av(Option, n_chan, Waveforms_HA_c) # Av. Waveforms
     if Option == 3: # No ref cluster, no indexes
-        indices_maxn = []      
-    TF.PlotWaveforms(Print_S_Opt, Option, Waveforms_HA_c, Waveforms_HA_c_av, Time, n_chan, indices_maxn)   
-    
+        indices_maxn = []
+    TF.PlotWaveforms(Print_S_Opt, Option, Waveforms_HA_c, Waveforms_HA_c_av, Time, n_chan, indices_maxn)
+
 else: # Invalid option
     print('Please select a valid option for plotting spike waveforms.')
 
 # Compute the Mahalanobis distance of every point to every distribution,
 # excluding the point from its own distribution to avoid biases (LOO Algorithm).
-    
+
 # Compute Metric
 Av_dist_mat = TF.compute_distmat(Proj, metric = 'mahalanobis')
 Av_dist_mat_sim = (Av_dist_mat + Av_dist_mat.transpose())/2
-Red_mat = pdist(Av_dist_mat_sim)   
-    
+Red_mat = pdist(Av_dist_mat_sim)
+
 # Plot Mahalanobis Distance Matrices
 #TF.plotMahala_mat(Av_dist_mat_sim, Option, Suboption, n_chan, Tot_Var)
-    
+
 Link_ward   = TF.plot_hierarchical_dend(Red_mat, hier_method = 'ward')
 np.save('Link_ward.npy',Link_ward)
-        
