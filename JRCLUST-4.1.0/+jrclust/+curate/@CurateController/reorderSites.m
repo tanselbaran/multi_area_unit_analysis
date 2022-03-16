051b35845626e597e2ca7d412c19ff12786deb7f
tanselbaran <yasart@ethz.ch>
2022-03-16 14:13:46 +0100

    Deleted the old version of the JRCLUST

--- JRCLUST-4.1.0/+jrclust/+curate/@CurateController/reorderSites.m
+++
@@ -1,5 +0,0 @@
-function reorderSites(obj, order)
-    %REORDERSITES Reorder sites according to specified geometry
-    [~,obj.spatial_idx] = sort(order);
-end
-
--- JRCLUST-4.1.0/+jrclust/+curate/@CurateController/unitIndex.m
+++
@@ -1,5 +0,0 @@
-function unitIndices = unitIndex(obj, unitIDs)
-%UNITINDEX Get index into showSubset of unitIDs specified
-unitIndices = find(ismember(obj.showSubset, unitIDs));
-end
-
